f1e9bb01216994826449b78719aacaa276492959
Vitor <vtr88@yahoo.com.br>
2026-08-18 19:40:04 -0300

    Update blog deployment workflow

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
-hugo --cleanDestinationDir && rsync -avz --delete -e 'ssh -i /home/soth/.config/.ssh/id_ed' /home/soth/Documentos/blog/public/ root@216.238.99.112:/var/www/html/
+#!/bin/sh
+set -eu
+
+cd -- "$(dirname -- "$0")"
+
+hugo --cleanDestinationDir
+rsync -avz --delete \
+  -e 'ssh -F /home/soth/.ssh/config -i /home/soth/.config/.ssh/id_ed' \
+  public/ root@216.238.99.112:/var/www/html/
